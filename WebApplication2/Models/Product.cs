f4c2d5d9b819857451801f185f8026ea5fd8784a
ArtesianBeliever <ArtesianBeliever@users.noreply.github.com>
2024-10-15 13:12:26 +0300

    fixes

--- WebApplication2/Models/Product.cs
+++ WebApplication2/Models/Product.cs
@@ -8,22 +8,25 @@ public class Product
     public Product()
     {
         ProductIngredients = new List<ProductIngredient>();
+        ProductCategories = new List<ProductCategory>();
     }
     public int ProductId { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
     public decimal Price { get; set; }
     public int Stock { get; set; }
-    public int CategoryId { get; set; }
+    //public int CategoryId { get; set; }
     
     [NotMapped]
     public IFormFile? ImageFile { get; set; }
     public string ImageUrl { get; set; } = "https://via.placeholder.com/150";
     
-    [ValidateNever]
-    public Category? Category { get; set; }
+    //[ValidateNever]
+    //public Category? Category { get; set; }
     [ValidateNever]
     public ICollection<OrderItem>? OrderItems { get; set; }
     [ValidateNever]
     public ICollection<ProductIngredient>? ProductIngredients  { get; set; }
+    [ValidateNever]
+    public ICollection<ProductCategory>? ProductCategories { get; set; }
 }
